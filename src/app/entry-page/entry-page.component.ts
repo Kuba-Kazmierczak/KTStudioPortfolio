65c7342de614f636b8a8a2e6e726ad9385bd8636
Tomasz Łaski <tomasz.laski0001@gmail.com>
2023-04-03 22:01:17 +0200

    add simple routing to web page

--- src/app/entry-page/entry-page.component.ts
+++ src/app/entry-page/entry-page.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-entry-page',
   templateUrl: './entry-page.component.html',
-  styleUrls: ['./entry-page.component.css']
+  styleUrls: ['./entry-page.component.css'],
 })
 export class EntryPageComponent {
-  links = [
-    {}
-  ];
+  links = [{ title: 'welcome', fragment: 'welcome' }];
   constructor(public route: ActivatedRoute) {}
-
-  
-  
 }
